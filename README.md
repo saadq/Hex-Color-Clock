7a230e09011b0af5f3fecd4cffb9b4836fa59eed
Saad Quadri <saadquadri95@gmail.com>
2014-12-30 20:32:29 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Hex Color Clock
+
+A simple program that changes the the current time into a hexadecimal color value which is then used as a background color. It is updated every second.
---
+++ js/hexclock.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  function hexClock() {
+    var clock = new Date();
+    var hours = clock.getHours(),
+        mins = clock.getMinutes(),
+        secs = clock.getSeconds();
+
+    if(hours <= 9) hours = '0' + hours;
+    if(mins <= 9) mins = '0' + mins;
+    if(secs <= 9) secs = '0' + secs;
+
+    var time = hours + ':' + mins + ':' + secs;
+    var color = '#' + hours + mins + secs;
+    document.body.style.background = color;
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('hexColor').innerHTML = color;
+
+    setTimeout(hexClock, 1000);
+  }
+
+  hexClock();
+}());
